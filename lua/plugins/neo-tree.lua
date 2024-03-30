9d82ff97860e598ec5f5b5bb67acaa60d8ec9507
Russ Gallaway <russgallaway@gmail.com>
2024-03-29 23:33:18 -0300

    changed lualine theme and neo-tree keymaps

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,18 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree toggle <CR>")
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      use_libuv_file_watcher = true,
+    })
+
+    vim.keymap.set("n", "<leader>ee", ":Neotree toggle <CR>")
+    vim.keymap.set("n", "<leader>ef", ":Neotree filesystem reveal left <CR>")
+    vim.keymap.set("n", "<leader>ec", ":Neotree close <CR>")
+  end,
 }
